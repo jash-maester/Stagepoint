a2c77b231e3ade557828f64badf0975270d42fdb
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2026-05-30 12:06:33 +0530

    Initial Commit

---
+++ Simple Teleprompter/Simple_TeleprompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TeleprompterApp.swift
+//  Simple Teleprompter
+//
+//  Created by Jashaswimalya Acharjee on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TeleprompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
